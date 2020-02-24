76795543011d5177c3f67a5bcd1295244eb002f7
Douglas Urner <dlu@canishe.com>
2020-02-24 11:28:59 -0800

    Check existance of repository.

--- assessment/auto-score.rb
+++ assessment/auto-score.rb
@@ -2,16 +2,38 @@
 
 # Automated checks
 
+require 'optparse'
+
 opts = {
-  tmp_dir: "tmp",
+  base_url: 'https://github.com/',
+  clone: 'git clone',
+  tmp_dir: 'tmp',
   verbose: false
 }
 
 OptionParser.new do |o|
   o.banner = "Usage: #{$0} [options]"
-  
-  o.on("-T TMPDIR") { |v| opts[:tmp_dir] = v }
-  o.on("-v")        { |v| opts[:verbose] = true }
+
+  o.on('-T TMPDIR') { |v| opts[:tmp_dir] = v }
+  o.on('-v')        { |v| opts[:verbose] = true }
 end.parse!
 
-p ARGV
+score = 0
+
+github_repo = ARGV.pop
+
+# Attempt to clone the repository.
+local_repo = github_repo.gsub(opts[:base_url], '')
+local_repo = "#{opts[:tmp_dir]}/#{local_repo}" if opts[:tmp_dir]
+repo_name = local_repo.gsub(/^.*\//, '')
+
+cmd = "#{opts[:clone]} #{github_repo} #{local_repo}"
+
+stdout = %x( #{cmd} )
+
+if ($?.exitstatus == 0)
+  # Success
+  score += 1
+end
+
+puts score
